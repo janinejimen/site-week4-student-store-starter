97e8a7a669c303332a1410ba86cd84fb3fd0c58a
janinejimen <jimenezj9@utexas.edu>
2025-06-23 16:15:38 -0700

    queries for products completed

--- student-store-api/controllers/student-store-controller.js
+++ student-store-api/controllers/student-store-controller.js
@@ -1,10 +1,38 @@
+//here is where the router methods are implemented, but this way of implementation is easier in the long term when implementing other features
+
 const prisma = require("../models/prisma-client");
 
+//not only gets all of the products available, this method also filters out products
+//if the filter is applicable
 exports.getAll = async (req, res) => {
-    const products = await prisma.product.findMany()
-    res.json(products)
+    //query because params is for route parameters and query is for query parameters
+    const {category, price, name} = req.query
+    let products = await prisma.product.findMany()
 
+    if(category) {
+        products = products.filter((p) => 
+            p.category.toLowerCase().includes(category.toLowerCase())
+        )
+
+    }
+
+     if(price) {
+        const parsedPrice = parseFloat(price)
+
+        if(!isNaN(parsedPrice)) {
+            products = products.filter((p)=> p.price === parsedPrice)
+        }
+    }
+
+
+    if(name) {
+        products = products.filter((p) => {
+            //why does this return thing fix the code?
+            return p.name.toLowerCase().includes(name.toLowerCase())
+        })
+    }
     
+    res.json(products)
 }
 
 exports.getById = async (req, res) => {
