9f91d3c81ca4d740b05e01cfc8bc152a88e62285
janinejimen <jimenezj9@utexas.edu>
2025-06-23 14:09:02 -0700

    created CRUD for product model

---
+++ student-store-api/controllers/student-store-controller.js
@@ -0,0 +1,47 @@
+const prisma = require("../models/prisma-client");
+
+exports.getAll = async (req, res) => {
+    const products = await prisma.product.findMany()
+    res.json(products)
+}
+
+exports.getById = async (req, res) => {
+    const id = Number(req.params.id)
+    const product = await prisma.product.findUnique({where: {id}})
+    if(!product) {
+        return res.status(404).json({error:"Not Found"})
+    }
+
+    res.json(product)
+}
+
+exports.create = async (req, res) => {
+    const {name, description, price, image_url, category} = req.body
+    const newProduct = await prisma.product.create({
+        data: {
+            name,
+            description,
+            price,
+            image_url,
+            category
+        }
+    })
+    res.status(201).json(newProduct)
+}
+
+exports.update = async (req, res) => {
+    const id = Number(req.params.id)
+    const {name, description, price, image_url, category} = req.body
+    const updatedProduct = await prisma.product.update ({
+        where: {id},
+        data: {name, description, price, image_url, category}
+    })
+
+    res.json(updatedProduct)
+}
+
+exports.remove = async (req, res) => {
+    const id = Number(req.params.id)
+    await prisma.product.delete({where: {id}})
+    res.status(204).end()
+}
--- student-store-api/src/server.js
+++ student-store-api/src/server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
     res.send('Hello, World! PROJECT IS WORKING')
+    res.send
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
