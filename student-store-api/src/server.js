0e98a754cd39e920bea40d61ef4c10fb8ae4bf62
janinejimen <jimenezj9@utexas.edu>
2025-06-23 13:32:08 -0700

    milestone 0 completed- m1 currently in progress

--- student-store-api/src/server.js
+++ student-store-api/src/server.js
@@ -1,1 +1,10 @@
+require("dotenv").config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT
 
+app.get('/', (req, res) => {
+    res.send('Hello, World! PROJECT IS WORKING')
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
