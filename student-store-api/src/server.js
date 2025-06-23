670daf75c221e612a82419508dda4f44a112e605
janinejimen <jimenezj9@utexas.edu>
2025-06-23 14:28:42 -0700

    POST router tested and fixed

--- student-store-api/src/server.js
+++ student-store-api/src/server.js
@@ -2,10 +2,13 @@ require("dotenv").config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT
+const productRoutes = require("../routes/student-store-routes.js")
 
 app.get('/', (req, res) => {
     res.send('Hello, World! PROJECT IS WORKING')
-    res.send
 })
+
+app.use(express.json());
+app.use("/products", productRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
