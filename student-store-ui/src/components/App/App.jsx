70a2e207576b8dd4bfb111f3c50969c48aefb4bd
janinejimen <jimenezj9@utexas.edu>
2025-06-26 06:20:16 -0700

    successfully pushed cart order into order route

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -6,16 +6,21 @@ import Sidebar from "../Sidebar/Sidebar";
 import Home from "../Home/Home";
 import ProductDetail from "../ProductDetail/ProductDetail";
 import NotFound from "../NotFound/NotFound";
-import { removeFromCart, addToCart, getQuantityOfItemInCart, getTotalItemsInCart } from "../../utils/cart";
+import {
+  removeFromCart,
+  addToCart,
+  getQuantityOfItemInCart,
+  getTotalItemsInCart,
+} from "../../utils/cart";
+import { calculateOrderSubtotal, calculateTotal } from "../../utils/calculations";
 import "./App.css";
 
 function App() {
-
   // State variables
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [activeCategory, setActiveCategory] = useState("All Categories");
   const [searchInputValue, setSearchInputValue] = useState("");
-  const [userInfo, setUserInfo] = useState({ name: "", dorm_number: ""});
+  const [userInfo, setUserInfo] = useState({ name: "", dorm_number: "" });
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
   const [isFetching, setIsFetching] = useState(false);
@@ -37,26 +42,67 @@ function App() {
   };
 
   const handleOnCheckout = async () => {
-  }
+    setIsCheckingOut(true);
+
+    try {
+      const customer_id = Number(userInfo.dorm_number);
+
+      const itemsForOrder = [];
+
+      //iterate through the cart and create an arrray of items for the order
+      for (const item in cart) {
+        const quantity = cart[item];
+        const productId = parseInt(item);
+
+        const product = products.find((p) => p.id === productId);
+
+        const itemPrice = product.price;
+
+        itemsForOrder.push({
+          product_id: product.id,
+          quantity: quantity,
+          price: itemPrice,
+        });
+      }
+
+      console.log(itemsForOrder)
+      const orderSubtotal = calculateOrderSubtotal(itemsForOrder);
+      const finalTotalPrice = calculateTotal(orderSubtotal);
+
+      console.log(finalTotalPrice)
+
+       const orderPush = {
+        customer_id: customer_id,
+        total_price: finalTotalPrice,
+        status: "completed",
+        created_at: new Date().toISOString(),
+        orderItems: itemsForOrder
+      }
+
+      const pushData = await axios.post("http://localhost:3000/orders", orderPush)
+
+      setCart({}); 
+      setUserInfo({name:"",dorm_number:""})
+    } catch {}
+    setIsCheckingOut(false)
+  };
 
   //use effect before return
   //this is how the website knows something happened and now we need to talk to the backend
-  useEffect (() => {
+  useEffect(() => {
     setIsFetching(true);
-      const fetchProducts = async() => {
-
-        try {
-          const response = await axios.get('http://localhost:3000/products');
-          setProducts(response.data);
-        } catch (error) {
-          setError("Error in fetching the products")
-        }
-        
-        setIsFetching(false);
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/products");
+        setProducts(response.data);
+      } catch (error) {
+        setError("Error in fetching the products");
       }
-      fetchProducts()
-  }, [])
 
+      setIsFetching(false);
+    };
+    fetchProducts();
+  }, []);
 
   return (
     <div className="App">
@@ -134,4 +180,3 @@ function App() {
 }
 
 export default App;
- 
